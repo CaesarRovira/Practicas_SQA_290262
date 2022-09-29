65bf73cdec23c28a3f7154fd1e5bff7f3473a6cf
César A. Rovira Montero <66651890+CaesarRovira@users.noreply.github.com>
2022-09-28 20:55:07 -0500

    Práctica 5 (2do Avance)

--- practica5.py
+++ practica5.py
@@ -43,3 +43,130 @@ pwd.send_keys("aeiou12345")
 
 user.send_keys(Keys.ENTER)
 
+# Selección de Categoría y Productos
+
+#-------
+# Adición de 1er celular al carrito
+
+sleep(2)
+# Selección de Categoría de celulares
+item = driver.find_element(By.CSS_SELECTOR, "a.list-group-item:nth-child(2)")
+item.click()
+
+sleep(2)
+
+item = driver.find_element(By.CSS_SELECTOR, "div.col-md-6:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1) > a:nth-child(1)")
+item.click()
+
+sleep(2)
+
+item = driver.find_element(By.CSS_SELECTOR, "a.btn")
+item.click()
+item.send_keys(Keys.ENTER)
+
+item = driver.find_element(By.CSS_SELECTOR, "li.nav-item:nth-child(1) > a:nth-child(1)")
+item.click()
+
+#--------
+# Adición de 2do Celular al carrito
+
+sleep(2)
+# Selección de Categoría de celulares
+item = driver.find_element(By.CSS_SELECTOR, "a.list-group-item:nth-child(2)")
+item.click()
+
+sleep(2)
+
+item = driver.find_element(By.CSS_SELECTOR, "div.col-lg-4:nth-child(2) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1) > a:nth-child(1)")
+item.click()
+
+sleep(2)
+
+item = driver.find_element(By.CSS_SELECTOR, "a.btn")
+item.click()
+item.send_keys(Keys.ENTER)
+
+item = driver.find_element(By.CSS_SELECTOR, "li.nav-item:nth-child(1) > a:nth-child(1)")
+item.click()
+
+#--------
+# Adición de 1er laptop
+
+sleep(2)
+# Selección de Categoría de laptops
+item = driver.find_element(By.CSS_SELECTOR, "a.list-group-item:nth-child(3)")
+item.click()
+
+sleep(2)
+
+item = driver.find_element(By.CSS_SELECTOR, "div.col-md-6:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1) > a:nth-child(1)")
+item.click()
+
+sleep(2)
+
+item = driver.find_element(By.CSS_SELECTOR, "a.btn")
+item.click()
+item.send_keys(Keys.ENTER)
+
+item = driver.find_element(By.CSS_SELECTOR, "li.nav-item:nth-child(1) > a:nth-child(1)")
+item.click()
+
+# Adición de 2da laptop
+sleep(2)
+# Selección de Categoría de laptops
+item = driver.find_element(By.CSS_SELECTOR, "a.list-group-item:nth-child(3)")
+item.click()
+
+sleep(2)
+
+item = driver.find_element(By.CSS_SELECTOR, "div.col-lg-4:nth-child(2) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1) > a:nth-child(1)")
+item.click()
+
+sleep(2)
+
+item = driver.find_element(By.CSS_SELECTOR, "a.btn")
+item.click()
+item.send_keys(Keys.ENTER)
+
+item = driver.find_element(By.CSS_SELECTOR, "li.nav-item:nth-child(1) > a:nth-child(1)")
+item.click()
+
+#-------------------------------------------------
+# Visualización del carrito y compra de objetos
+sleep(2)
+cart = driver.find_element(By.ID, "cartur")
+cart.click()
+
+cart = driver.find_element(By.CSS_SELECTOR, "button.btn:nth-child(3)")
+cart.click()
+
+# Formulario de Compra
+sleep(2)
+cart = driver.find_element(By.ID, "name")
+cart.send_keys('aeiou')
+
+cart = driver.find_element(By.ID, "country")
+cart.send_keys('Parangaticurimicuaro')
+
+cart = driver.find_element(By.ID, "city")
+cart.send_keys('Parangaticurimicuaro')
+
+cart = driver.find_element(By.ID, "card")
+cart.send_keys('Equisde')
+
+cart = driver.find_element(By.ID, "month")
+cart.send_keys('Equisde')
+
+cart = driver.find_element(By.ID, "year")
+cart.send_keys('Equisde')
+
+cart = driver.find_element(By.CSS_SELECTOR, "#orderModal > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(2)")
+cart.click()
+
+sleep(2)
+
+cart = driver.find_element(By.CSS_SELECTOR, ".confirm")
+cart.click()
+
+
+driver.quit()
